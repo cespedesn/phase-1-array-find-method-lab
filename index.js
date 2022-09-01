0a3bdf2918a8d66a21b5974938d8a080848ba7b0
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-09-01 12:49:42 -0400

    commit tree 32ab7c76ead0c688cba2f0026e88a0e8ccf1ef97

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// code your solution here
+const winsAndLosses = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+// const sadReality =  [ 
+//     { year: "2018", result: "N/A"},
+//     { year: "2017", result: "N/A"},
+//     { year: "2016", result: "N/A"},
+// ]
+
+
+function superbowlWin(winsAndLosses) {
+    const result = winsAndLosses.find(winsAndLosses => winsAndLosses.result === 'W');
+    return result ? result.year : undefined
+}
+
+superbowlWin(record);
+
+
+
+
+// function testFN(data) {
+//     data.forEach(element => {
+        
+//     })
+// }
+// console.log(record); 
+
+
+
+
+// function superbowlWin(winsAndLosses) {
+//     return winsAndLosses.find(aDub).year;
+//  }
+
+
+// function aDub(winsAndLosses) {
+//     if(winsAndLosses.result === 'W') {
+//         return true;
+//     } else {
+//         return undefined;
+//     }
+// }
+
+// function noDub(soSad) {
+//     if(sadReality.result === 'N/A') {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
+// function soSad(noDub) {
+//     if(sadReality.result === 'N/A') {
+//         return undefined;
+//     }
+// }
